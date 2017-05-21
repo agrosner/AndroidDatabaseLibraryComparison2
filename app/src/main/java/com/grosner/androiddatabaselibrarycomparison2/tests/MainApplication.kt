3699b2945c268aed8818bf710a92628b8e1a0fb2
Andrew Grosner <abgrosner@gmail.com>
2017-05-21 19:31:43 -0400

    [tests] fix rotation issues mostly. add LiveData and move tests to its own class.

--- app/src/main/java/com/grosner/androiddatabaselibrarycomparison2/tests/MainApplication.kt
+++ app/src/main/java/com/grosner/androiddatabaselibrarycomparison2/tests/MainApplication.kt
@@ -1,7 +1,5 @@
 package com.grosner.androiddatabaselibrarycomparison2.tests
 
-import android.app.Application
-import com.raizlabs.android.dbflow.config.FlowConfig
 import com.raizlabs.android.dbflow.config.FlowManager
 import io.realm.Realm
 
@@ -10,7 +8,7 @@ class MainApplication : android.app.Application() {
     override fun onCreate() {
         super.onCreate()
 
-        com.raizlabs.android.dbflow.config.FlowManager.init(com.raizlabs.android.dbflow.config.FlowConfig.Builder(this).build())
-        io.realm.Realm.init(this)
+        FlowManager.init(this)
+        Realm.init(this)
     }
 }
